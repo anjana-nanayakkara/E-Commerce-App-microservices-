39548c5dc450dd00a42d890c19327f05c88b0f14
“anjana <74060929+anjana-nanayakkara@users.noreply.github.com>
2024-07-01 23:45:21 +0530

    configur the discovery server config server and client service~

---
+++ services/customer/src/main/java/com/ecomapp/customer/exception/CustomerNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ecomapp.customer.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class CustomerNotFoundException extends RuntimeException {
+
+    private final String msg;
+}
